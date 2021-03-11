8a1b7712d4cb2009e5721e3112ef96ac3aa73ddc
Jeffery Forbes <jefferyforbes.1@outlook.com>
2021-03-11 15:18:02 +0000

    file refactor

--- mackims-reactjs/src/App.js
+++ mackims-reactjs/src/App.js
@@ -1,11 +1,27 @@
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Navbar />
+      <div className="Content">
+        <Switch>
+          <Route exact path="/">
+          <Home />
+          </Route>
+          <Route path="/gallery">
+          <Gallery />
+          </Route>
+          <Route path="*">
+            {/* The "*" path is for any page or endpoint that the user requests but does not exists. */}
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
